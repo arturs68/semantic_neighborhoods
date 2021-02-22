ff16dd5668fa7c674ac492b805d29681694d8163
Artur Szałata <arturs6668@gmail.com>
2021-02-22 13:11:12 +0100

    upload the training data to mlflow

--- train_cross_modal_retrieval_model.py
+++ train_cross_modal_retrieval_model.py
@@ -1,14 +1,13 @@
+import argparse
+
 import numpy as np
 import os
 import sys
-import random
-import glob
 import torch
-from PIL import Image
 from tqdm import tqdm
-# from torch.utils.tensorboard import SummaryWriter
 import csv, itertools
 import pandas as pd
+import mlflow
 
 csv.field_size_limit(sys.maxsize)
 
@@ -19,13 +18,27 @@ datasets_dict = {
                      "jsonl_prefix": "breaking_news"},
     "mscoco": {"data_dir": "/mnt/localdata/szalata/datasets/mscoco",
                "jsonl_prefix": "coco"},
+    "nytimes": {"data_dir": "/mnt/localdata/szalata/datasets/nytimes",
+                "jsonl_prefix": "nytimes800k"},
     # not extracted yet
     "politics": {},
-    "nytimes": {},
     "goodnews": {}
 }
 
 splits = ["train", "val", "test"]
+
+
+def set_seed(seed):
+    """
+    Set the seed for NumPy and pyTorch
+    :param args: Namespace
+        Arguments passed to the script
+    """
+    torch.random.manual_seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    torch.cuda.manual_seed_all(seed)
 
 
 class MyDataset(torch.utils.data.Dataset):
@@ -40,20 +53,23 @@ class MyDataset(torch.utils.data.Dataset):
         dataset_dict = datasets_dict[dataset_name]
         doc2vec_df = pd.read_pickle(os.path.join(dataset_dict["data_dir"], "doc2vec",
                                                       f"doc2vec_{dataset_dict['jsonl_prefix']}_{split}.pkl"))
-        self.article_vectors = doc2vec_df[2]
-        doc2vec_df = doc2vec_df.reset_index()[["index", 1]].explode(1)
+        self.article_vectors = np.array(doc2vec_df[2].tolist())
+        doc2vec_df["index"] = list(range(len(doc2vec_df)))
+        doc2vec_df = doc2vec_df[["index", 1]].explode(1)
         doc2vec_df["index"] = doc2vec_df["index"].astype("category")
         doc2vec_df[1] = doc2vec_df[1].astype("category")
-        self.id_path = doc2vec_df
-        self.image_vectors = pd.read_pickle(os.path.join(dataset_dict["data_dir"],
-                                                         f"{image_features_model}_{dataset_dict['jsonl_prefix']}_{split}.pkl"))
+        image_df = pd.DataFrame(pd.read_pickle(os.path.join(dataset_dict["data_dir"],
+                                                         f"{image_features_model}_{dataset_dict['jsonl_prefix']}_{split}.pkl"))).T
+        self.id_df = pd.merge(doc2vec_df.rename(columns={1: "path"}),
+                              image_df.reset_index().reset_index()[["level_0", "index"]], left_on="path", right_on="index").drop(columns=["index_y"])
+        self.image_vectors = image_df.values
 
     def __len__(self):
-        return len(self.id_path)
+        return len(self.id_df)
 
     def __getitem__(self, index):
-        entry = self.id_path.iloc[index]
-        return self.image_vectors[entry[1]].astype(np.float32), self.article_vectors.iloc[entry["index"]].astype(np.float32)
+        entry = self.id_df.iloc[index]
+        return self.image_vectors[entry["level_0"]], self.article_vectors[entry["index_x"]]
 
 
 class ImageProjectModel(torch.nn.Module):
@@ -135,74 +151,108 @@ class TripletLoss(torch.nn.Module):
 
 
 def main():
-    train_dataloader = torch.utils.data.DataLoader(dataset=MyDataset('train', "breakingnews"),
-                                                   batch_size=512, shuffle=True, num_workers=40)
-    test_dataloader = torch.utils.data.DataLoader(dataset=MyDataset('val', "breakingnews"),
-                                                  batch_size=1024, shuffle=False, num_workers=40)
-    # writer = SummaryWriter(f'models/')
+    parser = argparse.ArgumentParser()
+
+    # Required parameter
+    parser.add_argument("--output_dir", default="output", type=str,
+                        help="The output directory where the model predictions and checkpoints "
+                             "will be written.")
+
+    parser.add_argument("--dataset_name", type=str)
+    parser.add_argument("--learning_rate", default=1e-4, type=float,
+                        help="The initial learning rate for Adam.")
+    parser.add_argument("--weight_decay", default=1e-5, type=float,
+                        help="Weight deay if we apply some.")
+    parser.add_argument("--num_epochs", default=100, type=int,
+                        help="Total number of training epochs to perform.")
+    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
+    parser.add_argument("--batch_size", default=128, type=int, help="Batch size per GPU/CPU.")
+    parser.add_argument("--batch_size_eval", default=256, type=int, help="Batch size per GPU/CPU.")
+    parser.add_argument("--run_name", type=str, help="name of the mlflow run")
+    parser.add_argument("--overwrite_output_dir", action='store_true',
+                        help="Overwrite the content of the output directory")
+
+    args = parser.parse_args()
+    mlflow.set_experiment("article2image")
+    mlflow.start_run(run_name=args.run_name)
+    mlflow.log_params(vars(args))
+
+    if os.path.exists(args.output_dir) and os.listdir(
+            args.output_dir) and not args.overwrite_output_dir:
+        raise ValueError(
+            "Output directory ({}) already exists and is not empty. Use --overwrite_output_dir to "
+            "overcome.".format(
+                args.output_dir))
+
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+    best_model_img = os.path.join(args.output_dir, "best_model_img.model")
+    best_model_text = os.path.join(args.output_dir, "best_model_text.model")
+
+    set_seed(args.seed)
+    n_gpu = torch.cuda.device_count()
+    batch_size = args.batch_size * max(1, n_gpu)
+    batch_size_eval = args.batch_size * max(1, n_gpu)
+
+    train_dataloader = torch.utils.data.DataLoader(dataset=MyDataset('train', args.dataset_name),
+                                                   batch_size=batch_size, shuffle=True, num_workers=32)
+    test_dataloader = torch.utils.data.DataLoader(dataset=MyDataset('val', args.dataset_name),
+                                                  batch_size=batch_size_eval, shuffle=False, num_workers=4)
+
     img_model = torch.nn.DataParallel(ImageProjectModel()).cuda()
     text_model = torch.nn.DataParallel(TextProjectModel()).cuda()
     optimizer = torch.optim.Adam(
-        params=itertools.chain(img_model.parameters(), text_model.parameters()), lr=0.0001,
-        weight_decay=1e-5)
+        params=itertools.chain(img_model.parameters(), text_model.parameters()), lr=args.learning_rate,
+        weight_decay=args.weight_decay)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', verbose=True,
                                                            patience=5)
     itr = 0
     triplet_loss = TripletLoss()
     best_loss = sys.maxsize
-    for e in tqdm(range(1, 1000), ascii=True, desc='Epoch'):
+    for e in tqdm(range(1, args.num_epochs + 1), ascii=True, desc='Epoch'):
         img_model.train()
         text_model.train()
         with tqdm(total=len(train_dataloader), ascii=True, leave=False, desc='iter') as pbar:
             for i, (images, articles) in enumerate(train_dataloader):
                 itr += 1
                 optimizer.zero_grad()
-                image_projections = img_model(images.cuda())
-                article_projections = text_model(articles.cuda())
+                image_projections = img_model(images.float().cuda())
+                article_projections = text_model(articles.float().cuda())
                 loss = triplet_loss(image_projections, article_projections)
 
                 loss.backward()
                 optimizer.step()
-                # writer.add_scalar('data/train_loss', loss.item(), itr)
-                # writer.add_scalar('data/l_i2t', l_i2t.item(), itr)
+                if itr % 100 == 0:
+                    mlflow.log_metric("training loss", loss.item() / max((len(image_projections) * len(article_projections) - len(image_projections)), 1), itr)
 
                 pbar.update()
         img_model.eval()
         text_model.eval()
         losses = []
-        random.seed(9485629)
         with tqdm(total=len(test_dataloader), ascii=True, leave=False,
                   desc='eval') as pbar, torch.no_grad():
             for i, (images, articles) in enumerate(test_dataloader):
                 with torch.no_grad():
-                    image_projections = img_model(images.cuda())
-                    article_projections = text_model(articles.cuda())
+                    image_projections = img_model(images.float().cuda())
+                    article_projections = text_model(articles.float().cuda())
                     loss = triplet_loss(image_projections, article_projections)
 
-                # writer.add_scalar('data/train_loss', loss.item(), itr)
-                # writer.add_scalar('data/l_i2t', l_i2t.item(), itr)
+                pbar.update()
+                losses.append(loss.item() / max((len(image_projections) * len(article_projections) - len(image_projections)), 1))
 
                 pbar.update()
-                losses.append(loss.item())
-
-                pbar.update()
-        curr_loss = np.mean(losses)
-        # writer.add_scalar('data/val_loss', curr_loss, e)
-        scheduler.step(curr_loss)
+        test_loss = np.mean(losses)
+        mlflow.log_metric("test loss", test_loss, e)
+        scheduler.step(test_loss)
         # save only the best model
-        if curr_loss < best_loss:
-            best_loss = curr_loss
-            print(best_loss)
-        #     # delete prior models
-        #     prior_models = glob.glob(f'models/l_i2t_l_sym_{sys.argv[1].replace(".","_")}_l_img_{sys.argv[2].replace(".","_")}_l_text_{sys.argv[3].replace(".","_")}/*.pth')
-        #     for pm in prior_models:
-        #         os.remove(pm)
-        #     try:
-        #         torch.save(rnn_model.state_dict(), f'models/l_i2t_l_sym_{sys.argv[1].replace(".","_")}_l_img_{sys.argv[2].replace(".","_")}_l_text_{sys.argv[3].replace(".","_")}/rnn_model_{e}.pth')
-        #         torch.save(img_model.state_dict(), f'models/l_i2t_l_sym_{sys.argv[1].replace(".","_")}_l_img_{sys.argv[2].replace(".","_")}_l_text_{sys.argv[3].replace(".","_")}/img_model_{e}.pth')
-        #     except:
-        #         print('Failed saving')
-        #         continue
+        if test_loss < best_loss:
+            best_loss = test_loss
+            if os.path.exists(best_model_img):
+                os.remove(best_model_img)
+            if os.path.exists(best_model_text):
+                os.remove(best_model_text)
+            torch.save(img_model.state_dict(), best_model_img)
+            torch.save(text_model.state_dict(), best_model_text)
 
 
 if __name__ == '__main__':
